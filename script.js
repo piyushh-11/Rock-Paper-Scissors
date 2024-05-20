c736898358b569d3f66325d27b444f994712826a
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-05-20 16:38:33 -0500

    one round implementation

--- script.js
+++ script.js
@@ -23,20 +23,28 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose! Paper beats Rock");
+        computerScore++;
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You win! Rock beats Scissors");
+        humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You win! Paper beats Rock");
+        humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lose! Scissors beats Paper");
+        computerScore++;
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lose! Rock beats Scissors");
+        computerScore++;
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You win! Scissors beats Paper");
+        humanScore++;
     } else {
         console.log("Draw. You both chose " + computerChoice);
     }
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
