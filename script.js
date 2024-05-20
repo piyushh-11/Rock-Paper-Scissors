610e55b899271ad2ce4a7b42090ec054101786c9
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-05-20 16:08:56 -0500

    playRound function

--- script.js
+++ script.js
@@ -19,7 +19,23 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock");
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors");
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper");
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors");
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper");
+    } else {
+        console.log("Draw. You both chose " + computerChoice);
+    }
 }
 
 console.log(getComputerChoice())
